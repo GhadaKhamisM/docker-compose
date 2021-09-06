29f63be89b0a006f901526c12c6ca86af95f86bc
GhadaKamisM <ghada.khamis@ibtikar.net.sa>
2021-09-06 16:54:10 +0200

    add check before delete service

--- laravel-app/app/Http/Controllers/ServiceController.php
+++ laravel-app/app/Http/Controllers/ServiceController.php
@@ -41,7 +41,7 @@ class ServiceController extends Controller
 
     public function destroy(Service $service){
         if($service->doctors()->count()){
-            abort(Response::HTTP_CONFLICT,'Can\'t delete this service, there are doctors related with it');
+            abort(Response::HTTP_METHOD_NOT_ALLOWED,'Can\'t delete this service, there are doctors related with it');
         }
         $this->serviceService->delete($service);
         return response()->json(['results' => null, 'messages' => 'Service deleted successfully'] , Response::HTTP_OK);
