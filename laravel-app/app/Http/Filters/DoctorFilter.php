e39736a7a26df70e7e7a23429568108aa391759e
GhadaKamisM <ghada.khamis@ibtikar.net.sa>
2021-09-07 16:43:25 +0200

    add pagination to filter scope

--- laravel-app/app/Http/Filters/DoctorFilter.php
+++ laravel-app/app/Http/Filters/DoctorFilter.php
@@ -29,4 +29,28 @@ class DoctorFilter extends QueryFilter
     {
         $this->builder->where('time_slot',$timeSlot);
     }
+
+    /**
+     * Sort the services by the given order and field.
+     *
+     * @param  array  $value
+     */
+    public function sort(string $value)
+    {
+        [$field,$order] = explode(',', $value);
+        $this->builder->orderBy($field, $order);
+    }
+
+    /**
+     * Paginate the services by the given limit and field.
+     *
+     * @param  array  $value
+     */
+    public function pagination(string $value)
+    {
+        [$pageSize,$pageNumber] = explode(',', $value);
+        $count = count($this->builder->get());
+        $skip  = ($pageNumber - 1) * $pageSize;
+        $this->builder->skip($skip)->take($pageSize);
+    }
 }
--- laravel-app/app/Http/Filters/ServiceFilter.php
+++ laravel-app/app/Http/Filters/ServiceFilter.php
@@ -21,4 +21,28 @@ class ServiceFilter extends QueryFilter
     {
         $this->builder->whereRaw('name like ?', array('%'.$description.'%'));
     }
+
+    /**
+     * Sort the services by the given order and field.
+     *
+     * @param  array  $value
+     */
+    public function sort(string $value)
+    {
+        [$field,$order] = explode(',', $value);
+        $this->builder->orderBy($field, $order);
+    }
+
+    /**
+     * Paginate the services by the given limit and field.
+     *
+     * @param  array  $value
+     */
+    public function pagination(string $value)
+    {
+        [$pageSize,$pageNumber] = explode(',', $value);
+        $count = count($this->builder->get());
+        $skip  = ($pageNumber - 1) * $pageSize;
+        $this->builder->skip($skip)->take($pageSize);
+    }
 }
