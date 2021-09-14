5cbaafe51530b88aff2aac426d5582ff676067c9
GhadaKamisM <ghada.khamis@ibtikar.net.sa>
2021-09-14 16:00:44 +0200

    check doctor days hour at edit and store if ther is any overlapping

--- laravel-app/app/Http/Services/DoctorService.php
+++ laravel-app/app/Http/Services/DoctorService.php
@@ -25,6 +25,7 @@ class DoctorService
     }
 
     public function create(array $data){
+        $this->validateDoctorWeekDays($data['doctor_week_days']);
         $data['photo'] = $this->uploadFile($data['photo'],'/doctors');
         return $this->doctorRepository->create($data);
     }
@@ -34,6 +35,7 @@ class DoctorService
     }
 
     public function update(Doctor $doctor,array $data){
+        $this->validateDoctorWeekDays($data['doctor_week_days']);
         $data['photo'] = $this->uploadFile($data['photo'],'/doctors');
         $this->doctorRepository->update($doctor,$data);
     }
@@ -77,5 +79,34 @@ class DoctorService
             }
         }
         return $availableTimes;
+    }
+
+    private function validateDoctorWeekDays($doctorWeekDays){
+        $duplicatedWeekDays = $this->getDuplicatedWeekDays($doctorWeekDays);    
+        if(empty($duplicatedWeekDays)){
+            return ;
+        }
+        $doctorWeekDays = collect($doctorWeekDays)->whereIn('week_day_id',array_keys($duplicatedWeekDays))->sortBy('start_hour');
+        return $this->checkTimeOverlapping(array_values($doctorWeekDays->toArray()));
+    }
+
+    private function getDuplicatedWeekDays($doctorWeekDays){
+        $weekDaysIds = array_column($doctorWeekDays,'week_day_id');
+        $countOfrepeatedWeekDays = array_count_values($weekDaysIds);
+        return array_filter($countOfrepeatedWeekDays, function ($count) { return $count > 1; });
+    }
+
+    private function checkTimeOverlapping($doctorWeekDays){
+        foreach ($doctorWeekDays as $key => $doctorWeekDay) {
+            $weekDayId = $doctorWeekDay['week_day_id'];
+            $duplicatedDoctorWeekDays = array_filter($doctorWeekDays, function ($doctorWeekDay) use ($weekDayId) { return $doctorWeekDay['week_day_id'] == $weekDayId; });
+            $doctorWeekDayIndex = array_search($doctorWeekDay,$duplicatedDoctorWeekDays);
+            if($doctorWeekDayIndex > 0){
+                $previousEndTime = $duplicatedDoctorWeekDays[$doctorWeekDayIndex -1]['to_hour'];
+                if($doctorWeekDay['start_hour'] < $previousEndTime){
+                    abort(Response::HTTP_UNPROCESSABLE_ENTITY,Lang::get('messages.doctors.errors.week_days'));
+                }
+            }
+        }
     }
 }
